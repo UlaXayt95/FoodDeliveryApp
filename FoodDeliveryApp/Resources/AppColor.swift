fc7c19195b7bfcc57046d3c324783757ae576aee
Ulugbek <UlaXayt95gmail.com>
2024-08-12 00:00:39 +0300

    Added new base colors

---
+++ FoodDeliveryApp/Resources/AppColor.swift
@@ -0,0 +1,15 @@
+//
+//  AppColor.swift
+//  FoodDeliveryApp
+//
+//  Created by Улугбек Хайтметов on 11.08.2024.
+//
+
+import UIKit
+
+enum AppColor {
+    static let accentOrange = UIColor().hex(0xD35400)
+    static let grey = UIColor().hex(0xECF0F1)
+    static let black = UIColor().hex(0x00000)
+    static let background = UIColor().hex(0xE5E5E5)
+}
